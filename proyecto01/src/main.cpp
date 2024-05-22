e94075599758ad394f01807fa26af8a3a4d3280c
asoleal <jlealgom@unal.edu.co>
2024-05-22 10:20:38 -0500

    Subiendo proyecto01 con cmake

---
+++ proyecto01/src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+
+int main(){
+	std::cout<<"Hola mundo!"<<std::endl;
+	std::cout<<"Let's go"<<std::endl;
+    std::cout<<"Raiz de dos= "<<sqrt(2.0)<<std::endl;
+	return 0;
+} 
